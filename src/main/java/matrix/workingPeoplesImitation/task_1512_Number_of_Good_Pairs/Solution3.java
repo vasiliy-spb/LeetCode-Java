5d653d6fb47e52e03c052796ff3f46eb7c289d72
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-03-22 21:16:06 +0700

    fixed code style

--- src/main/java/matrix/workingPeoplesImitation/task_1512_Number_of_Good_Pairs/Solution3.java
+++ src/main/java/matrix/workingPeoplesImitation/task_1512_Number_of_Good_Pairs/Solution3.java
@@ -3,7 +3,8 @@ package matrix.workingPeoplesImitation.task_1512_Number_of_Good_Pairs;
 // from leetcode code sample (2)
 public class Solution3 {
     public int numIdenticalPairs(int[] nums) {
-        int count = 0, cnt[] = new int[101];
+        int count = 0;
+        int[] cnt = new int[101];
         for (int num : nums) {
             count += cnt[num]++;
         }
