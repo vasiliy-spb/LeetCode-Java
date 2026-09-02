0b13e140010e9d7a7ba8cf5073687cf99e31d695
vasiliy-spb <vasiliy-spb07@mail.ru>
2026-09-02 18:39:59 +0700

    solved 02.09.2026

---
+++ src/main/java/matrix/workingPeoplesImitation/task_3875_Construct_Uniform_Parity_Array_I/Solution.java
@@ -0,0 +1,8 @@
+package matrix.workingPeoplesImitation.task_3875_Construct_Uniform_Parity_Array_I;
+
+// my solution
+public class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
