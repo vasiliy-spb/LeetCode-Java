a3bf526da694f371427bafc333c21aae069730df
vasiliy-spb <vasiliy-spb07@mail.ru>
2026-09-08 19:05:33 +0700

    solved 08.09.2026

---
+++ src/main/java/matrix/workingPeoplesImitation/task_3870_Count_Commas_in_Range/Solution.java
@@ -0,0 +1,8 @@
+package matrix.workingPeoplesImitation.task_3870_Count_Commas_in_Range;
+
+// my solution
+public class Solution {
+    public int countCommas(int n) {
+        return Math.max(n - 999, 0);
+    }
+}
